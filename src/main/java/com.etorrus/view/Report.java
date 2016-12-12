1c4ada68eaeb3dfa0454cf937894c060bb65ac7f
Etor <etorrus@gmail.com>
2016-12-12 11:40:09 +0500

    Now the report saves documents for each author in a separate file in the format JSON

--- src/main/java/com.etorrus/view/Report.java
+++ src/main/java/com.etorrus/view/Report.java
@@ -4,29 +4,50 @@ import com.etorrus.model.document.Document;
 import com.etorrus.model.staff.Person;
 import com.etorrus.wrapper.DocumentWrapperManager;
 import com.etorrus.wrapper.PersonWrapperManager;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**Отчет должен содержать перечень авторов документов
- * и список документов созданных ими*/
+ * и список документов созданных ими
+ * и сохранять отчет по каждому автору в отдельный файл
+ * в формате json*/
 
 public class Report {
 
     public void getReport(){
 
-        System.out.println("==========REPORT==========");
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
 
+        System.out.println("==========REPORT==========");
         List<Person> personList = PersonWrapperManager.getPersonList();
         List<Document> documentList = DocumentWrapperManager.getDocumentList();
 
+        String json;
+
         for (Person author : personList){
             System.out.println(author.getLastName() + " " + author.getFirstName() + " " + author.getPatronymic());
+            List<Document> reportAuthor = new ArrayList<Document>();
             for (Document document : documentList){
-                //System.out.println(document.getName() + "От " + document.getDateReg());
 
                 if (author.getId().equals(document.getAuthor().getId())){
+                    reportAuthor.add(document);
                     System.out.println("        " + document.getName() + "От " + document.getDateReg());
                 }
+            }
+            json = gson.toJson(reportAuthor);
+            try {
+                FileWriter writer = new FileWriter("E:/xmlDocument/" + author.getLastName() + ".json");
+                writer.write(json);
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
