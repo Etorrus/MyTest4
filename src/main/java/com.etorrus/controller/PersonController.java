4588bf84b51eda55d10d489fb1ac7e20659beaac
Etor <etorrus@gmail.com>
2016-12-13 13:50:52 +0500

    added /employees page

--- src/main/java/com.etorrus/controller/PersonController.java
+++ src/main/java/com.etorrus/controller/PersonController.java
@@ -1,7 +1,30 @@
 package com.etorrus.controller;
 
+import com.etorrus.model.staff.Person;
+import com.etorrus.service.PersonService;
+import com.etorrus.wrapper.PersonWrapper;
+import com.etorrus.wrapper.PersonWrapperManager;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
 /**
  *
  */
+@Path(value="/employees")
 public class PersonController {
+
+    PersonWrapperManager personWrapperManager = new PersonWrapperManager();
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Person> getPersons()
+    {
+
+        List<Person> listOfPersons = personWrapperManager.getPersonList();
+        return listOfPersons;
+    }
 }
--- src/main/java/com.etorrus/service/PersonService.java
+++ src/main/java/com.etorrus/service/PersonService.java
@@ -1,8 +1,10 @@
 package com.etorrus.service;
 
+import com.etorrus.dao.JaxbParser;
 import com.etorrus.model.staff.Person;
 import com.etorrus.wrapper.PersonWrapper;
 
+import java.io.File;
 import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -11,14 +13,11 @@ import javax.ws.rs.Produces;
 /**
  *
  */
-@Path(value="/ecm/employees")
+
 public class PersonService {
 
-    @Produces({"application/json"})
-    @GET
-    public List<Person> getPersons(){
-        PersonWrapper personWrapper = new PersonWrapper();
-        return personWrapper.getPersonList();
-    }
+    /*public List<Person> getAllPersons(){
+
+    }*/
 
 }
